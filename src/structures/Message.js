9360f062251cc7ca8b3ef90cc791e416196a4cb5
Bianca Carvalho <biancapcarvalho@outlook.com>
2022-11-25 13:41:13 -0300

    fix quoted msg

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -301,14 +301,18 @@ class Message extends Base {
      * Returns the quoted message, if any
      * @returns {Promise<Message>}
      */
-    async getQuotedMessage() {
+     async getQuotedMessage() {
         if (!this.hasQuotedMsg) return undefined;
 
         const quotedMsg = await this.client.pupPage.evaluate((msgId) => {
             let msg = window.Store.Msg.get(msgId);
-            return msg.quotedMsgObj().serialize();
+            let quotedMsgObj = ('quotedMsgObj' in msg) ? msg.quotedMsgObj() : msg.getRawQuotedMsgObj();
+            if (!quotedMsgObj) return undefined;
+
+            return quotedMsgObj.serialize();
         }, this.id._serialized);
 
+        if (!quotedMsg) return undefined;
         return new Message(this.client, quotedMsg);
     }
 
